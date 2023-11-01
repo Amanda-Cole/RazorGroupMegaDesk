975ed66e2e4afe72472481a1608b0d79e54c0c25
Kendra Bryant <kbryant@efilecabinet.com>
2023-10-31 21:12:38 -0600

    Redirect on initial load

--- RazorGroupMegaDesk/Pages/Index.cshtml.cs
+++ RazorGroupMegaDesk/Pages/Index.cshtml.cs
@@ -12,9 +12,9 @@ namespace RazorGroupMegaDesk.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
-
+            return RedirectToPage("/Orders/Index");
         }
     }
 }
